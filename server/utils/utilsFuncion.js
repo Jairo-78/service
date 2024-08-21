8f90fe6105f78fd2fc01c68ca35738a1fd37ccf2
Rapiwash <sistemarapiwash@gmail.com>
2024-08-20 22:30:43 -0500

    correciones y mejoras

--- server/utils/utilsFuncion.js
+++ server/utils/utilsFuncion.js
@@ -1,9 +1,26 @@
 import Servicio from "../models/portafolio/servicios.js";
 import Categoria from "../models/categorias.js";
-import Factura from "../models/Factura.js";
 import Anular from "../models/anular.js";
-import Pagos from "../models/pagos.js";
-import { handleGetInfoUser } from "../routes/cuadreDiario.js";
+import { nameDelivery } from "./varsGlobal.js";
+
+// Función para agrupar objetos usando una clave de identificación
+export function mapObjectByKey(array, key) {
+  return array.reduce((map, obj) => {
+    map[obj[key]] = obj;
+    return map;
+  }, {});
+}
+
+// Función para agrupar objetos en arrays usando una clave de identificación
+export function mapArrayByKey(array, key) {
+  return array.reduce((map, obj) => {
+    if (!map[obj[key]]) {
+      map[obj[key]] = [];
+    }
+    map[obj[key]].push(obj);
+    return map;
+  }, {});
+}
 
 export const handleGetInfoDelivery = async () => {
   try {
@@ -24,7 +41,7 @@ export const handleGetInfoDelivery = async () => {
     // Consulta a la colección Servicio
     const servicio = await Servicio.findOne({
       idCategoria: categoriaId,
-      nombre: "Delivery",
+      nombre: nameDelivery,
     });
 
     // Verifica si se encontró el servicio
@@ -35,25 +52,6 @@ export const handleGetInfoDelivery = async () => {
     return servicio;
   } catch (error) {
     console.error("Error al buscar el servicio:", error);
-  }
-};
-
-export const GetOrderId = async (id) => {
-  try {
-    // Buscar el documento por su ID
-    const factura = await Factura.findById(id);
-
-    // Verificar si se encontró el documento
-    if (!factura) {
-      console.log("No se encontró ninguna factura con ese ID");
-      return null; // o puedes lanzar un error según tus necesidades
-    }
-
-    // Devolver el documento encontrado
-    return factura;
-  } catch (error) {
-    console.error("Error al buscar la factura:", error);
-    throw error; // puedes manejar el error según tus necesidades
   }
 };
 
@@ -73,77 +71,5 @@ export const GetAnuladoId = async (id) => {
   } catch (error) {
     console.error("Error al buscar el registro anulado:", error);
     throw error; // puedes manejar el error según tus necesidades
-  }
-};
-
-export const GetPagoMasDetalleOrden = async (idPago) => {
-  try {
-    const pagoInfo = await Pagos.findById(idPago);
-
-    const factura = await Factura.findById(pagoInfo.idOrden);
-
-    const detallePago = {
-      _id: pagoInfo._id,
-      idUser: pagoInfo.idUser,
-      orden: factura.codRecibo,
-      idOrden: pagoInfo.idOrden,
-      date: pagoInfo.date,
-      nombre: factura.Nombre,
-      total: pagoInfo.total,
-      metodoPago: pagoInfo.metodoPago,
-      Modalidad: factura.Modalidad,
-      isCounted: pagoInfo.isCounted,
-      infoUser: await handleGetInfoUser(pagoInfo.idUser),
-    };
-    return detallePago;
-  } catch (error) {
-    console.error("Error al obtener los datos por _id de pago:", error);
-    throw error; // Propagar el error para que sea manejado por el llamador
-  }
-};
-
-export const GetListPagosMasDetalleOrden = async (idOrden) => {
-  try {
-    // Buscar la factura por su id
-    const factura = await Factura.findById(idOrden);
-
-    if (!factura) {
-      throw new Error("Factura no encontrada");
-    }
-
-    // Obtener los IDs de los pagos asociados a la factura
-    const pagosIds = factura.listPago;
-
-    // Buscar los detalles de cada pago usando los IDs
-    const ListDetallePagos = await Promise.all(
-      pagosIds.map(async (pagoId) => {
-        const pago = await Pagos.findById(pagoId);
-        if (!pago) {
-          throw new Error(`Pago con ID ${pagoId} no encontrado`);
-        }
-        const infoUser = await handleGetInfoUser(pago.idUser);
-        return {
-          _id: pago._id,
-          idUser: pago.idUser,
-          orden: factura.codRecibo,
-          idOrden: pago.idOrden,
-          date: pago.date,
-          nombre: factura.Nombre,
-          total: pago.total,
-          metodoPago: pago.metodoPago,
-          Modalidad: factura.Modalidad,
-          isCounted: pago.isCounted,
-          infoUser: infoUser,
-        };
-      })
-    );
-
-    return ListDetallePagos;
-  } catch (error) {
-    console.error(
-      "Error al obtener los detalles de pago por id de orden:",
-      error
-    );
-    throw error;
   }
 };
